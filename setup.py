c540af784ed57795c389c718504ec8b05d6dd6b5
Patrick Ayoup <patrick.ayoup@gmail.com>
2019-05-16 01:20:42 -0400

    Added release deployment

--- setup.py
+++ setup.py
@@ -6,15 +6,21 @@ def read(file_name):
     return open(os.path.join(os.path.dirname(__file__), file_name)).read()
 
 
+# TODO: Include a release command which will validate
+# the version file against tags before tagging
 setup(
     name='pybunpro',
-    version='1.0.0',
+    version=read('.VERSION'),
     author='Patrick Ayoup',
     author_email='patrick.ayoup@gmail.com',
+    url='http://patrickayoup.github.io/pybunpro',
     description='REST client for Bunpro',
     license='MIT',
     keywords='rest client japanese grammar',
     packages=['pybunpro'],
+    entry_points={
+        'console_scripts': ['pybunpro=pybunpro.__main__:cli']
+    },
     install_requires=['requests',
                       'marshmallow',
                       'Click'],
